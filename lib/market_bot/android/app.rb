74e3354b83ea1955ece1c4d984b9a12f38f91216
Chad Remesch <chad@remesch.com>
2016-05-13 16:05:08 -0700

    WIP cleaning up selectors (remove xpath).

--- lib/market_bot/android/app.rb
+++ lib/market_bot/android/app.rb
@@ -51,15 +51,15 @@ module MarketBot
 
           case field_name
           when 'Updated'
-            result[:updated] = info.css('.content').text.strip
+            result[:updated] = info.at_css('.content').text.strip
           when 'Installs'
-            result[:installs] = info.css('.content').text.strip
+            result[:installs] = info.at_css('.content').text.strip
           when 'Size'
-            result[:size] = info.css('.content').text.strip
+            result[:size] = info.at_css('.content').text.strip
           when 'Current Version'
-            result[:current_version] = info.css('.content').text.strip
+            result[:current_version] = info.at_css('.content').text.strip
           when 'Requires Android'
-            result[:requires_android] = info.css('.content').text.strip
+            result[:requires_android] = info.at_css('.content').text.strip
           when 'Contact Developer', 'Developer'
             info.css('.dev-link').each do |node|
               node_href = node[:href]
@@ -77,30 +77,29 @@ module MarketBot
           end
         end
 
-        result[:content_rating] = doc.css("div.content[itemprop='contentRating']").text
+        result[:content_rating] = doc.at_css("div.content[itemprop='contentRating']").text
 
-        node = doc.xpath("//meta[@itemprop='price']").first
-        result[:price] = node[:content].strip rescue 'Free'
+        result[:price] = doc.at_css('meta[itemprop="price"]')[:content]
 
-        category_div = doc.css('.category').first
+        category_div = doc.at_css('.category')
         result[:category] = category_div.text.strip rescue ''
         cat_link = category_div["href"]
         path, cat_name = File.split(cat_link)
         result[:category_url] = cat_name
 
-        result[:description] = doc.xpath("//div[@itemprop='description']").first.inner_html.strip
-        result[:title] = doc.css('div.id-app-title').text
+        result[:description] = doc.at_css('div[itemprop="description"]').inner_html.strip
+        result[:title] = doc.at_css('div.id-app-title').text
 
-        score = doc.css('.score-container').first
+        score = doc.at_css('.score-container')
         unless score.nil?
-          node  = score.css('.score').first
+          node  = score.at_css('.score')
           result[:rating] = node.text.strip
-          node = score.xpath("//meta[@itemprop='ratingCount']").first
+          node = score.at_css('meta[itemprop="ratingCount"]')
           result[:votes] = node[:content].strip
         end
 
-        node = doc.xpath("//div[@itemprop='author']")
-        result[:developer] = node.css('.primary').first.text.strip
+        node = doc.at_css('div[itemprop="author"]')
+        result[:developer] = node.at_css('.primary').text.strip
 
         result[:more_from_developer] = []
         result[:users_also_installed] = []
@@ -108,7 +107,7 @@ module MarketBot
 
         node = doc.css('.recommendation')
         node.css('.rec-cluster').each do |recommended|
-          assoc_app_type = recommended.css('.heading').first.text.strip.eql?('Similar' ) ? :related : :more_from_developer
+          assoc_app_type = recommended.at_css('.heading').text.strip.eql?('Similar' ) ? :related : :more_from_developer
           recommended.css('.card').each do |card|
             assoc_app = {}
             assoc_app[:app_id] = card['data-docid'].strip
@@ -119,7 +118,7 @@ module MarketBot
         # Users also installed is no longer on the page, adding this for backwards compatibility, well, sort of....
         result[:users_also_installed] = result[:related]
 
-        node = doc.css('.cover-image').first
+        node = doc.at_css('.cover-image')
         unless node.nil?
           url = fix_content_url(node[:src])
           result[:banner_icon_url] = url
@@ -136,7 +135,7 @@ module MarketBot
           result[:full_screenshot_urls] << fix_content_url(node[:src])
         end
 
-        node = doc.css('.whatsnew').first
+        node = doc.at_css('.whatsnew')
         result[:whats_new] = node.inner_html.strip unless node.nil?
 
         # Stubbing out for now, can't find them in the redesigned page.
@@ -146,8 +145,8 @@ module MarketBot
         unless options[:skip_reviews] # Review parsing is CPU intensive.
           doc.css('.single-review').each do |node|
             review = {}
-            review[:author_name] = node.css('.author-name').text.strip if node.css('.author-name')
-            raw_tag = node.css('.current-rating').to_s
+            review[:author_name] = node.at_css('.author-name').text.strip if node.at_css('.author-name')
+            raw_tag = node.at_css('.current-rating').to_s
             if raw_tag.match(/100%;/i)
               review[:review_score] = 5
             elsif raw_tag.match(/80%;/i)
@@ -159,13 +158,13 @@ module MarketBot
             elsif raw_tag.match(/20%;/i)
               review[:review_score] = 1
             end
-            if node.css('.review-title')
-              review[:review_title] = node.css('.review-title').text.strip
+            if node.at_css('.review-title')
+              review[:review_title] = node.at_css('.review-title').text.strip
             end
-            if node.css('.review-body')
-              review[:review_text] = node.css('.review-body').text
+            if node.at_css('.review-body')
+              review[:review_text] = node.at_css('.review-body').text
                 .sub!(review[:review_title],'')
-                .sub!(node.css('.review-link').text,'')
+                .sub!(node.at_css('.review-link').text, '')
                 .strip
             end
             if review
@@ -179,7 +178,7 @@ module MarketBot
         histogram = doc.css('div.rating-histogram')
         cur_index = 5
         %w(five four three two one).each do |slot|
-          node = histogram.css(".#{slot.to_s}")
+          node = histogram.at_css(".#{slot.to_s}")
           result[:rating_distribution][cur_index] = node.css('.bar-number').text.gsub(/,/,'').to_i
           cur_index -= 1
 
